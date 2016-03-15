d07502402802d6cf4696529bd1198322401accab
“HuoYongJie” <349574068@qq.com>
2016-03-15 18:29:35 +0800

    no message

---
+++ YJUIKit/YJUIKit/UIButton/Controller/UIButton7ViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIButton7ViewController.h
+//  YJUIKit
+//
+//  Created by hyj on 16/3/15.
+//  Copyright © 2016年 juyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton7ViewController : UIViewController
+
+@end
---
+++ YJUIKit/YJUIKit/UIButton/Controller/UIButton7ViewController.m
@@ -0,0 +1,37 @@
+//
+//  UIButton7ViewController.m
+//  YJUIKit
+//
+//  Created by hyj on 16/3/15.
+//  Copyright © 2016年 juyi. All rights reserved.
+//
+
+#import "UIButton7ViewController.h"
+
+@interface UIButton7ViewController ()
+
+@end
+
+@implementation UIButton7ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
